e19d6d5bce699c107d43e18bedd8b57e2f9567e2
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-14 05:28:31 +0900

    initial commit

---
+++ src/main/java/com/example/google/google_hackathon/GoogleHackathonApplication.java
@@ -0,0 +1,13 @@
+package com.example.google.google_hackathon;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GoogleHackathonApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GoogleHackathonApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/google/google_hackathon/GoogleHackathonApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.google.google_hackathon;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GoogleHackathonApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
