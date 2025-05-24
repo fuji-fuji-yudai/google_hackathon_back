fb9d818bced9a30a097ab4cb17baddce211ca770
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-24 13:58:45 +0900

    ガチでログイン

--- src/main/java/com/example/google/google_hackathon/config/SecurityConfig.java
+++ src/main/java/com/example/google/google_hackathon/config/SecurityConfig.java
@@ -8,6 +8,8 @@ import org.springframework.security.config.annotation.authentication.configurati
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
 import org.springframework.security.config.http.SessionCreationPolicy;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.security.web.authentication.UsernamePasswordAuthenticationFilter;
 
 import com.example.google.google_hackathon.security.JwtAuthenticationFilter;
@@ -44,4 +46,11 @@ public class SecurityConfig {
     public AuthenticationManager authenticationManager(AuthenticationConfiguration config) throws Exception {
         return config.getAuthenticationManager();
     }
+
+    
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+    return new BCryptPasswordEncoder();
+    }
+
 }
