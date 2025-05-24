1461ae8b6f4a072ec2c8dca4f77b97cb13da9f18
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-24 12:23:54 +0900

    jwt

--- src/main/java/com/example/google/google_hackathon/config/SecurityConfig.java
+++ src/main/java/com/example/google/google_hackathon/config/SecurityConfig.java
@@ -3,6 +3,7 @@ import jakarta.servlet.http.HttpServletResponse;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.config.Customizer;
 import org.springframework.security.config.annotation.authentication.configuration.AuthenticationConfiguration;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.web.SecurityFilterChain;
@@ -23,6 +24,7 @@ public class SecurityConfig {
     @Bean
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
         return http
+            .cors(Customizer.withDefaults())
             .csrf(csrf -> csrf.disable())
             .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
             .authorizeHttpRequests(auth -> auth
