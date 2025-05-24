77226df1a0c43ea8414442536ceaadc0bd8874e1
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-24 13:47:15 +0900

    jwt

--- src/main/java/com/example/google/google_hackathon/controller/LoginApiController.java
+++ src/main/java/com/example/google/google_hackathon/controller/LoginApiController.java
@@ -8,6 +8,7 @@ import org.springframework.security.authentication.UsernamePasswordAuthenticatio
 import org.springframework.security.core.Authentication;
 import org.springframework.security.core.AuthenticationException;
 import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -28,6 +29,8 @@ public class LoginApiController {
     @Autowired
     private JwtTokenProvider jwtTokenProvider;
 
+    
+    @CrossOrigin(origins = "http://localhost:8080", allowCredentials = "true")
     @PostMapping("/login")
     public ResponseEntity<?> login(@RequestBody LoginRequest loginRequest) {
     try {
