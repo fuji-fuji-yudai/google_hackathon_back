0eacf51251fb6d3349ea3bd08bff4a51276fd08e
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-31 17:32:07 +0900

    ユーザー識別

--- src/main/java/com/example/google/google_hackathon/security/JwtUtil.java
+++ src/main/java/com/example/google/google_hackathon/security/JwtUtil.java
@@ -7,13 +7,17 @@ import io.jsonwebtoken.security.Keys;
 import java.nio.charset.StandardCharsets;
 import java.security.Key;
 
+import javax.crypto.spec.SecretKeySpec;
+
 public class JwtUtil {
     private static final String SECRET_KEY = "a-very-long-and-secure-secret-key-that-is-at-least-64-bytes-long-1234567890!@#$%^&*()"; // 32文字以上
+    
 
     
     public static boolean validateToken(String token) {
     try {
-    Jwts.parserBuilder().setSigningKey(SECRET_KEY.getBytes(StandardCharsets.UTF_8)).build()
+        Key key = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "HmacSHA512");
+        Jwts.parserBuilder().setSigningKey(key).build()
 .parseClaimsJws(token);
     return true;
     } catch (Exception e) {
@@ -21,12 +25,17 @@ public class JwtUtil {
     }
     }
 
+
 public static String getUsernameFromToken(String token) {
-return Jwts.parser()
-            .setSigningKey(SECRET_KEY)
-            .parseClaimsJws(token)
-            .getBody()
-            .getSubject();
- }
+ Key key = new SecretKeySpec(SECRET_KEY.getBytes(StandardCharsets.UTF_8), "HmacSHA512");
+
+ return Jwts.parserBuilder()
+ .setSigningKey(key)
+ .build()
+ .parseClaimsJws(token)
+ .getBody()
+ .getSubject();
+}
+
 }
 
