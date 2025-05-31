66cb7297608254f0ebcebdb5fbfe459e8144460f
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-05-31 11:29:24 +0900

    ユーザー識別

--- src/main/java/com/example/google/google_hackathon/config/WebSocketConfig.java
+++ src/main/java/com/example/google/google_hackathon/config/WebSocketConfig.java
@@ -24,7 +24,7 @@ public class WebSocketConfig implements WebSocketMessageBrokerConfigurer {
             .withSockJS(); // SockJS を有効化
   }
 
-  
+  @Override
   public void configureClientInboundChannel(ChannelRegistration registration) {
     registration.interceptors(new JwtChannelInterceptor());
   }
