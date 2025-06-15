25d49916352fc083db63bdd15b114c58c6130559
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-06-15 15:07:38 +0900

    ログ追加

--- src/main/java/com/example/google/google_hackathon/entity/Room.java
+++ src/main/java/com/example/google/google_hackathon/entity/Room.java
@@ -12,8 +12,10 @@ import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.Table;
 
 @Entity
+@Table(name = "rooms",schema = "public")
 public class Room {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
