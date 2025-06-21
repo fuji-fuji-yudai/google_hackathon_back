fc105dcb09f43477d0a8fc8a0b6702a47404d75c
Toshinori Mori <toshinori.mori@ibm.com>
2025-06-21 16:29:38 +0900

    森森

--- src/main/java/com/example/google/google_hackathon/controller/TaskManageController.java
+++ src/main/java/com/example/google/google_hackathon/controller/TaskManageController.java
@@ -12,6 +12,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import com.example.google.google_hackathon.security.JwtUtil;
 import java.util.List;
+import java.security.Principal;
 
 @RestController
 // Spring Framework において、HTTPリクエストとコントローラのメソッドやクラスを紐づけるアノテーションです。
@@ -23,26 +24,14 @@ public class TaskManageController {
     private TaskService taskService;
 
     @GetMapping
-    public ResponseEntity<List<TaskDto>> getTasks(@RequestHeader("Authorization") String authHeader) {
-        String token = authHeader.replace("Bearer ", "");
-        if (!JwtUtil.validateToken(token)) {
-            return ResponseEntity.status(401).build();
-        }
-
+    public ResponseEntity<List<TaskDto>> getTasks(Principal principal) {
+        System.out.println("認証済みユーザー: " + principal.getName());
         return ResponseEntity.ok(taskService.getAllTasks());
     }
 
     @PostMapping
-    public ResponseEntity<Void> updateTasks(
-            @RequestBody List<TaskDto> tasks,
-            @RequestHeader("Authorization") String authHeader) {
-                System.out.println("受け取った Authorization ヘッダー: " + authHeader); // ← ここ
-        String token = authHeader.replace("Bearer ", "");
-        if (!JwtUtil.validateToken(token)) {
-            System.out.println("JWTが無効: " + token); // ← ここ
-            return ResponseEntity.status(401).build();
-        }
-
+    public ResponseEntity<Void> updateTasks(@RequestBody List<TaskDto> tasks, Principal principal) {
+        System.out.println("認証済みユーザー: " + principal.getName());
         taskService.updateTasks(tasks);
         return ResponseEntity.ok().build();
     }
