791205828fd68463762aeb24023455d5f9996c09
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-06-24 21:45:29 +0900

    ロードマップ作成案をAIが提示

--- src/main/java/com/example/google/google_hackathon/dto/RoadmapSuggestRequestDto.java
+++ src/main/java/com/example/google/google_hackathon/dto/RoadmapSuggestRequestDto.java
@@ -3,6 +3,14 @@ package com.example.google.google_hackathon.dto;
 public class RoadmapSuggestRequestDto {
     private String period;
     private String category;
+    public String getCategory() {
+        
+        return period;
+    }
+    public String getPeriod() {
+        
+        return category;
+    }
 
     // getter/setter
 }
