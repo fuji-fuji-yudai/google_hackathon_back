dead497a713f318418b6bccf3c747f0301b2552a
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-06-28 08:50:22 +0900

    jacksonの上書き

---
+++ src/main/java/com/example/google/google_hackathon/config/JacksonConfig.java
@@ -0,0 +1,18 @@
+package com.example.google.google_hackathon.config;
+
+import org.springframework.boot.autoconfigure.jackson.Jackson2ObjectMapperBuilderCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+
+@Configuration
+public class JacksonConfig {
+    @Bean
+    public Jackson2ObjectMapperBuilderCustomizer customizer() {
+        return builder -> {
+            builder.modules(new JavaTimeModule());
+            builder.featuresToDisable(SerializationFeature.WRITE_DATES_AS_TIMESTAMPS);
+        };
+    }
+}
