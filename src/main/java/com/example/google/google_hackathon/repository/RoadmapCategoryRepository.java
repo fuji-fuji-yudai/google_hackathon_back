8317eedba5bf22bf59e5b2661280017e0a7e8d65
Yudai Fujieda <Yudai.Fujieda@ibm.com>
2025-06-28 18:22:14 +0900

    カテゴリ取得

--- src/main/java/com/example/google/google_hackathon/repository/RoadmapCategoryRepository.java
+++ src/main/java/com/example/google/google_hackathon/repository/RoadmapCategoryRepository.java
@@ -2,12 +2,21 @@ package com.example.google.google_hackathon.repository;
 
 import com.example.google.google_hackathon.entity.RoadmapCategory;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
+import java.util.List;
 import java.util.Optional;
 
 @Repository
 public interface RoadmapCategoryRepository extends JpaRepository<RoadmapCategory, Long> {
     // user_id と name でRoadmapCategoryを検索するメソッド
     Optional<RoadmapCategory> findByUser_IdAndName(Long userId, String name);
+
+    @Query("SELECT DISTINCT SUBSTRING(rc.name, 1, LOCATE(' - ', rc.name) - 1) FROM RoadmapCategory rc WHERE rc.user.id = :userId")
+    List<String> findDistinctCategoryNamesByUserId(@Param("userId") Long userId);
+
 }
+
+
